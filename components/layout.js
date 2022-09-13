f35fa8ef7f55fe531b4ba9e7e208e44430f08c1a
Josh Arroyo <josh@prismatec.co>
2022-09-13 10:41:01 -1000

    :fire: Minor home and style cleanup

--- components/layout.js
+++ components/layout.js
@@ -31,13 +31,6 @@ export default function Layout({ children, home }) {
         )}
       </header>
       <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
     </div>
   );
 }
